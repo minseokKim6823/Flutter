70681481d7e3ffae8bce2f9299a9ea270844d441
minseokKim6823 <alstjr6823@naver.com>
2023-10-05 10:16:35 +0900

    input slide img

---
+++ week5/lib/homescreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class HomeScreen extends StatelessWidget{
+  const HomeScreen({Key? key}): super(key: key);
+  @override
+  Widget build(BuildContext context){
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
+    return Scaffold(
+      body: PageView(
+        children: [1,2,3,4,5]
+          .map(
+              (number) => Image.asset('asset/img/img$number.jpg',
+              fit:BoxFit.cover,
+              ),
+        ).toList(),
+      ),
+    );
+  }
+}
