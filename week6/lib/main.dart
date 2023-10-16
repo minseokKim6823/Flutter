1296d3f23ce33da26130b6e5885a7a40fcf911db
minseokKim6823 <alstjr6823@naver.com>
2023-10-16 23:40:15 +0900

    week6

---
+++ week6/lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:week6/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+          fontFamily: 'sunflower',
+          textTheme:const TextTheme(
+            displayLarge: const TextStyle(
+                color: Colors.white,
+                fontSize: 30.0
+            ),
+            displaySmall: TextStyle(
+              color: Colors.white,fontSize: 25.0,
+              fontWeight: FontWeight.w700,
+            ),
+            bodyLarge: TextStyle(
+              color: Colors.white,fontSize: 30.0,
+            ),
+            bodySmall: TextStyle(
+              color:Colors.white,fontSize: 20.0,
+            ),
+          )
+      ),
+      home: HomeScreen(),
+    ),
+  );
+}
