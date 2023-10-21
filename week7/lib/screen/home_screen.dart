2bf9ed3ed1d8439d8d22054cdfe238ddd0a2685c
minseokKim6823 <alstjr6823@naver.com>
2023-10-21 16:50:12 +0900

    .

--- week7/lib/screen/home_screen.dart
+++ week7/lib/screen/home_screen.dart
@@ -1,63 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
-//import 'package:week7/component/custom_video_player';
-class HomeScreen extends StatefulWidget{
-  const HomeScreen({Key? key}): super(key: key);
+import 'package:week7/component/custom_video.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen>{
+class _HomeScreenState extends State<HomeScreen> {
   XFile? video;
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
 
-      body: video ==null ? renderEmpty(): renderVideo(),
+      body: video == null ? renderEmpty() : renderVideo(),
     );
   }
-  Widget renderEmpty(){
+
+  Widget renderEmpty() {
     return Container(
-      width: MediaQuery.of(context).size.width,
+      width: MediaQuery
+          .of(context)
+          .size
+          .width,
       decoration: getBoxDecoration(),
-      child:Column(
+      child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           _Logo(
             onTap: onNewVideoPressed,
           ),
-          SizedBox(height:30.0),
+          SizedBox(height: 30.0),
           _AppName(),
         ],
       ),
     );
   }
-  void onNewVideoPressed() async{
+
+  void onNewVideoPressed() async {
     final video = await ImagePicker().pickVideo(
       source: ImageSource.gallery,
     );
-    if(video != null){
+    if (video != null) {
       setState(() {
-        this.video=video;
+        this.video = video;
       });
     }
   }
-  Widget renderVideo(){
+
+  Widget renderVideo() {
     return Center(
       child: CustomVideoPlayer(
-        video:video!, //video 주소
+        video: video!, //video 주소
+        onNewVideoPressed: onNewVideoPressed,
       ),
     );
   }
-  BoxDecoration getBoxDecoration(){
+
+  BoxDecoration getBoxDecoration() {
     return BoxDecoration(
       gradient: LinearGradient(
         begin: Alignment.topCenter,
         end: Alignment.bottomCenter,
-        colors:[
+        colors: [
           Color(0xFF2A3A7C),
           Color(0xFF000118),
         ],
@@ -69,14 +78,15 @@ class _HomeScreenState extends State<HomeScreen>{
 class _Logo extends StatelessWidget {
   final GestureTapCallback onTap;
 
-  const _Logo({required this.onTap, Key? key}) :super(key: key);//required this.onTap 생성자의 매개 변수
+  const _Logo({required this.onTap, Key? key})
+      :super(key: key); //required this.onTap 생성자의 매개 변수
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Image.asset(
-        'asset/img/logo.png',
+      'asset/img/logo.png',
       ),
     );
   }
@@ -86,14 +96,14 @@ class _AppName extends StatelessWidget {
   const _AppName({Key? key}) : super(key: key);
 
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     final textStyle = TextStyle(
       color: Colors.white,
-      fontSize:30.0,
+      fontSize: 30.0,
       fontWeight: FontWeight.w300,
     );
     return Row(
-      mainAxisAlignment:MainAxisAlignment.center ,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Text(
           'VIDEO',
