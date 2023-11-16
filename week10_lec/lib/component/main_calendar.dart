ad9ca35eddd839ba6c2c17c38559d34e6712239e
minseokKim6823 <alstjr6823@naver.com>
2023-11-16 22:00:00 +0900

    calendar 기본 틀

---
+++ week10_lec/lib/component/main_calendar.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:week10_lec/const/colors.dart';
+
+class MainCalendar extends StatelessWidget {
+  final OnDaySelected onDaySelected;
+  final DateTime selectedDate;
+
+  MainCalendar({
+    required this.onDaySelected,
+    required this.selectedDate,
+  });
+  @override
+  Widget build(BuildContext context){
+    return TableCalendar(
+      onDaySelected: onDaySelected, // ➌ 날짜 선택 시 실행할 함수
+      selectedDayPredicate: (date) => // ➍ 선택된 날짜를 구분할 로직
+      date.year == selectedDate.year &&
+          date.month == selectedDate.month &&
+          date.day == selectedDate.day,
+      firstDay: DateTime(1900,1,1),
+      lastDay: DateTime(2200,12,31),
+      focusedDay: DateTime.now(),
+      headerStyle: HeaderStyle(
+        titleCentered: true,
+        formatButtonVisible: false,
+        titleTextStyle: TextStyle(
+          fontWeight: FontWeight.w700,
+          fontSize: 16.0,
+        ),
+      ),
+      calendarStyle: CalendarStyle(
+        isTodayHighlighted: true,
+        defaultDecoration: BoxDecoration( // ➋ 기본 날짜 스타일
+          borderRadius: BorderRadius.circular(6.0),
+          color: LIGHT_GREY_COLOR,
+        ),
+        weekendDecoration: BoxDecoration( // ➌ 주말 날짜 스타일
+          borderRadius: BorderRadius.circular(6.0),
+          color: LIGHT_GREY_COLOR,
+        ),
+        selectedDecoration: BoxDecoration( // ➍ 선택된 날짜 스타일
+          borderRadius: BorderRadius.circular(6.0),
+          border: Border.all(
+            color: PRIMARY_COLOR,
+            width: 1.0,
+          ),
+        ),
+        defaultTextStyle: TextStyle( // ➎ 기본 글꼴
+          fontWeight: FontWeight.w600,
+          color: DARK_GREY_COLOR,
+        ),
+        weekendTextStyle: TextStyle( // ➏ 주말 글꼴
+          fontWeight: FontWeight.w600,
+          color: DARK_GREY_COLOR,
+        ),
+        selectedTextStyle: TextStyle( // ➐ 선택된 날짜 글꼴
+          fontWeight: FontWeight.w600,
+          color: PRIMARY_COLOR,
+        ),
+      ),
+    );
+  }
+}
--- week10_lec/lib/screen/home_screen.dart
+++ week10_lec/lib/screen/home_screen.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:week10_lec/component/main_calendar.dart';
 
-class HomeScreen extends StatelessWidget{
-  const HomeScreen({Key? Key}):super(key:key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) :super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>{
+  DateTime selectedDate = DateTime.utc( // ➋ 선택된 날짜를 관리할 변수
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
 
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Text('Home Screen'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            MainCalendar(
+                selectedDate: selectedDate,
+                onDaySelected: onDaySelected,
+            ),
+          ],
+        ),
+      )
     );
+  }
+  void onDaySelected(DateTime selectedDate, DateTime focusedDate){// ➌ 날짜 선택될 때마다 실행할 함수
+    setState(() {
+      this.selectedDate = selectedDate;
+    });
   }
 }
